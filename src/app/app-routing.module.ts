cf34f45a0da76938689125fbadb7a071b66f638d
Nowshad S K <00005786@arisglobal.com>
2021-12-04 08:24:59 +0530

    This is angular project is sample work on router, child router, nested router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LaptopComponent } from './product/laptop/laptop.component';
+import { MobileComponent } from './product/mobile/mobile.component';
+import { ProductComponent } from './product/product.component';
+import { ShirtComponent } from './product/shirt/shirt.component';
+import { TshirtComponent } from './product/tshirt/tshirt.component';
+import {LandingPageComponent} from './landing-page/landing-page.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'landing-page', pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'product',component:ProductComponent, children:[
+    
+    {path:'laptop',component:LaptopComponent},
+    {path:'mobile',component:MobileComponent},
+    {path:'shirt',component:ShirtComponent},
+    {path:'tshirt',component:TshirtComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { LaptopComponent } from './product/laptop/laptop.component';
+import { MobileComponent } from './product/mobile/mobile.component';
+import { ShirtComponent } from './product/shirt/shirt.component';
+import { TshirtComponent } from './product/tshirt/tshirt.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProductComponent,
+    LaptopComponent,
+    MobileComponent,
+    ShirtComponent,
+    TshirtComponent,
+    LandingPageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
